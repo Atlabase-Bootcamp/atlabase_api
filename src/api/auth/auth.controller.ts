475881f88fa713d4bdef7adfc9766f49998ab07c
GerardoVollmer <@geravollmer17@gmail.com>
2025-11-13 16:11:40 -0300

    API-8

---
+++ src/api/auth/auth.controller.ts
@@ -0,0 +1,24 @@
+import type { Request, Response } from "express";
+import * as authService from "@/src/api/auth/auth.service.js";
+import { ApiError, httpStatus } from "@@/utils/api.error.js";
+import { success } from "@/src/utils/response.helper.js";
+
+// llamamos a la funcion que registra un nuevo usuario
+export const registerHandler = async (req: Request, res: Response) => {
+  try {
+    const user = await authService.registerUser(req.body);
+    return success(res, user, httpStatus.CREATED);
+  } catch (error) {
+    return res.status(400).json({ error: (error as Error).message});
+  }
+};
+
+// llamamos a la funcion que loguea un usuario
+export const loginHandler = async (req: Request, res: Response) => {
+  try {
+    const token = await authService.loginUser(req.body);
+    return success(res, token);
+  } catch (error) {
+    return res.status(401).json({ error: (error as Error).message });
+  }
+};
